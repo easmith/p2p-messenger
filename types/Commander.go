53013f3db8e27e1df20a6035724886ff96a6116e
Евгений Кузнецов <easmith@mail.ru>
2018-11-27 22:46:25 +0300

    Первые шаги

---
+++ types/Commander.go
@@ -0,0 +1,24 @@
+package types
+
+var itHttp = map[string]bool{
+	"GET ": true,
+	"HEAD": true,
+	"POST": true,
+}
+
+var itMessanger = map[string]bool{
+	"HAND": true,
+	"MMSG": true,
+	"CLOS": true,
+}
+
+
+
+func ItIsHttp(ba []byte) bool {
+	return itHttp[string(ba)]
+}
+
+
+func ItIsMessanger(ba []byte) bool {
+	return itMessanger[string(ba)]
+}
