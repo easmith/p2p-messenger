64f2723b00f2ba804343ed7886388664a437610d
Евгений Кузнецов <easmith@mail.ru>
2018-11-28 02:50:57 +0300

    Return static content

--- server.go
+++ server.go
@@ -1,27 +1,32 @@
 /* ThreadedEchoServer
-*/
+ */
 package main
 
 import (
-	"bytes"
+	"bufio"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"local/p2pmessager/types"
 	"log"
 	"net"
+	"net/http"
 	"os"
+	"path"
+	"strings"
+	"time"
 )
 
 var name string
 
 func main() {
 
-	name = *flag.String("name", "defaultName", "name")
+	log.SetOutput(os.Stdout)
+
+	name = *flag.String("name", "ONE", "name")
 	port := flag.Int("port", 35035, "port as port")
 
 	flag.Parse()
-
-	print(name)
 
 	if *port <= 0 || *port > 65535 {
 		*port = 35035
@@ -29,7 +34,7 @@ func main() {
 
 	service := fmt.Sprintf(":%v", *port)
 
-	tcpAddr, err := net.ResolveTCPAddr("tcp", service )
+	tcpAddr, err := net.ResolveTCPAddr("tcp", service)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "ResolveTCPAddr: %s", err.Error())
 		os.Exit(1)
@@ -41,7 +46,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Printf("Start listen %s\n", service)
+	log.Println("Start listen " + service)
 
 	go SocketClient("", 1)
 
@@ -53,7 +58,6 @@ func main() {
 		go onConnection(conn)
 	}
 
-
 }
 
 func SocketClient(ip string, port int) {
@@ -63,68 +67,102 @@ func SocketClient(ip string, port int) {
 	defer conn.Close()
 
 	if err != nil {
-		log.Fatalln(err)
+		log.Fatalln("asdasd" + err.Error())
 	}
 
 	conn.Write([]byte("HAND" + name))
 
-	//buff := make([]byte, 1024)
-	//_, _ := conn.Read(buff)
-	//log.Printf("Receive: %s", buff[:n])
+	buff := make([]byte, 128)
+	n, err := conn.Read(buff)
+	log.Printf("Receive(%v): %s, %v\n", n, buff[:n], err)
 
 }
-
 
 func onConnection(conn net.Conn) {
 	defer conn.Close()
 
-	var buf [512]byte
-listener:
+	reader := bufio.NewReader(conn)
+	writer := bufio.NewWriter(conn)
+
+	readWriter := bufio.NewReadWriter(reader, writer)
+
+	buf, err := readWriter.Peek(4)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "ERROR: %s", err)
+		return
+	}
+
+	if types.ItIsMessanger(buf) {
+		log.Println("This is message!")
+		handleMessage(readWriter)
+	} else {
+		log.Println("Try request")
+		handleRequest(readWriter)
+	}
+}
+
+func handleMessage(rw *bufio.ReadWriter) {
+	var buf = make([]byte, 128)
 	for {
-		_, err := conn.Read(buf[0:])
+		n, err := rw.Read(buf)
 		if err != nil {
 			return
 		}
+		log.Printf("MAIN Recieved: [%v] %s (%s)", n, buf, err)
 
-		head := buf[:4]
-		if types.ItIsHttp(head) {
-			//readRequest, e := http.ReadRequest(buf)
+		nn, err := rw.Write([]byte("NAMEOK"))
+		log.Printf("MAIN Sent: [%v] (%s)", nn, err)
+		rw.Writer.Flush()
 
-			request := types.Parse(buf[0:])
-			fmt.Printf("%s\n", request.Verb)
-			fmt.Printf("%s\n", request)
-			writePage(conn)
-			break listener
-		}
-
-		if types.ItIsMessanger(head) {
-			switch {
-			case bytes.Equal([]byte("HAND"), head) : {
-				handshake(buf[4:16], conn)
-			}
-			case bytes.Equal([]byte("CLOS"), head) : {
-				break listener
-			}
-			}
-		}
+		time.Sleep(100000)
 	}
 }
 
-func writePage(conn net.Conn) {
-	response := types.New("hello!")
-
-	fmt.Printf("Response\n%s\n", response)
-
-	_, err := fmt.Fprintf(conn, "%s", response)
+func handleRequest(rw *bufio.ReadWriter) {
+	request, err := http.ReadRequest(rw.Reader)
 	if err != nil {
+		fmt.Fprintf(os.Stderr, "ERROR: %s", err)
 		return
 	}
+
+	log.Printf("Request: %v\t%v\n", request.URL.Path, path.Clean(request.URL.Path))
+
+	//fs := http.FileServer(http.Dir("/home/eku/go/src/local/p2pmessager/static/"))
+
+	response := http.Response{
+		StatusCode: 200,
+		ProtoMajor: 1,
+		ProtoMinor: 1,
+	}
+
+	file, e := os.Open("./static" + path.Clean(request.URL.Path))
+	if e != nil {
+		log.Printf("error: %s", e)
+		response.StatusCode = 404
+		response.Body = ioutil.NopCloser(strings.NewReader("Not found"))
+		response.Write(rw)
+		rw.Writer.Flush()
+		return
+	}
+
+	response.Body = file
+	response.Write(rw)
+
+	rw.Writer.Flush()
 }
 
-func handshake(handName []byte, conn net.Conn) {
-	fmt.Printf("Handshake from: %s", handName)
-	_, err := fmt.Fprintf(conn, "HAND%s", name)
-	if err != nil {
-		return
-	}
+type MyWriter struct {
+	*bufio.Writer
+}
+
+func (w MyWriter) Header() http.Header {
+	return http.Header{}
+}
+
+func (w MyWriter) WriteHeader(statusCode int) {
+	w.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK")))
+}
+
+func NewWriter(w *bufio.Writer) http.ResponseWriter {
+	return &MyWriter{w}
 }
