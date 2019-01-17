f507f6419ffd93fe7699e749a4f605d5564539c3
Евгений Кузнецов <easmith@mail.ru>
2019-01-17 05:22:08 +0300

    discover peer over UDP multicast

--- discover/discover.go
+++ discover/discover.go
@@ -2,36 +2,51 @@ package discover
 
 import (
 	"bufio"
+	"bytes"
+	"encoding/hex"
+	"fmt"
 	"github.com/easmith/p2p-messenger/proto"
 	"log"
 	"net"
-	"os"
+	"time"
 )
+
+var peers = make(map[string]string)
 
 //StartDiscover Начинает подключения к пирам из списка peers.txt и отправляет им свое имя
 func StartDiscover(p *proto.Proto, peersFile string) {
 
-	file, err := os.Open(peersFile)
-	if err != nil {
-		log.Printf("DISCOVER: Open peers.txt error: %s", err)
-		return
-	}
+	go meower("224.0.0.1:35035", p)
+	go meowListener("224.0.0.1:35035", p, connectToPeer)
 
-	var lastPeers []string
-
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		lastPeers = append(lastPeers, scanner.Text())
-	}
-
-	log.Printf("DISCOVER: Start peer discovering. Last seen peers: %v", len(lastPeers))
-	for _, peerAddress := range lastPeers {
-		go connectToPeer(p, peerAddress)
-	}
+	//file, err := os.Open(peersFile)
+	//if err != nil {
+	//	log.Printf("DISCOVER: Open peers.txt error: %s", err)
+	//	return
+	//}
+	//
+	//var lastPeers []string
+	//
+	//scanner := bufio.NewScanner(file)
+	//for scanner.Scan() {
+	//	lastPeers = append(lastPeers, scanner.Text())
+	//}
+	//
+	//log.Printf("DISCOVER: Start peer discovering. Last seen peers: %v", len(lastPeers))
+	//for _, peerAddress := range lastPeers {
+	//	go connectToPeer(p, peerAddress)
+	//}
 }
 
 // подключаемся к пиру по адресу
 func connectToPeer(p *proto.Proto, peerAddress string) {
+	if _, exist := peers[peerAddress]; exist {
+		log.Printf("peer %s already exist", peerAddress)
+		return
+	}
+	peers[peerAddress] = peerAddress
+	log.Printf("try to connect peer: %s", peerAddress)
+
 	conn, err := net.Dial("tcp", peerAddress)
 	if err != nil {
 		log.Printf("Dial ERROR: " + err.Error())
@@ -53,7 +68,73 @@ func connectToPeer(p *proto.Proto, peerAddress string) {
 
 	p.UnregisterPeer(peer)
 
+	delete(peers, peerAddress)
 	// TODO: ping-pong
+}
+
+func meower(address string, p *proto.Proto) {
+	addr, err := net.ResolveUDPAddr("udp", address)
+	if err != nil {
+		log.Printf(err.Error())
+	}
+
+	conn, err := net.DialUDP("udp", nil, addr)
+	if err != nil {
+		log.Printf(err.Error())
+	}
+
+	for {
+		_, err := conn.Write([]byte(fmt.Sprintf("meow:%v:%v", hex.EncodeToString(p.PubKey), p.Port)))
+		if err != nil {
+			log.Printf(err.Error())
+		}
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func meowListener(address string, p *proto.Proto, handler func(p *proto.Proto, peerAddress string)) {
+	// Parse the string address
+	addr, err := net.ResolveUDPAddr("udp", address)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Open up a connection
+	conn, err := net.ListenMulticastUDP("udp", nil, addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = conn.SetReadBuffer(1024)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Loop forever reading from the socket
+	for {
+		buffer := make([]byte, 1024)
+		_, src, err := conn.ReadFromUDP(buffer)
+		if err != nil {
+			log.Fatal("ReadFromUDP failed:", err)
+		}
+		trim := bytes.Trim(buffer, "\x00")
+
+		peerPubKeyStr := string(trim[5 : 5+64])
+		peerPubKey, err := hex.DecodeString(peerPubKeyStr)
+
+		// Если с этим пиром уже есть связь, то пропускаем его
+		_, found := p.Peers.Get(string(peerPubKey))
+		if found || bytes.Equal(p.PubKey, peerPubKey) {
+			continue
+		}
+
+		peerAddress := src.IP.String() + string(trim[5+64:])
+
+		log.Printf(" peer: %v %v", peerPubKeyStr, found)
+		log.Printf("Found peer: %s", peerAddress)
+
+		handler(p, peerAddress)
+	}
 }
 
 // Отправляем пиру свое имя и ожидаем от него его имя
@@ -75,4 +156,29 @@ func handShake(p *proto.Proto, conn net.Conn) *proto.Peer {
 	}
 
 	return peer
+}
+
+func iface() {
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		log.Printf(err.Error())
+		return
+	}
+	var addrs = make(map[string]net.Interface)
+	for _, i := range ifaces {
+		multicastAddrs, err := i.MulticastAddrs()
+		if err != nil {
+			log.Printf(err.Error())
+			continue
+		}
+		for _, a := range multicastAddrs {
+			addrs[a.String()] = i
+			log.Printf("%v\n", a.String())
+		}
+	}
+
+	log.Printf("=====")
+	for k, _ := range addrs {
+		log.Printf("%v", k)
+	}
 }
